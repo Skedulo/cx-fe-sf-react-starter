2e4b654fd734d9d1f129ec0c6c5f00ccbee95a8f
Loc Tran <loctran@Locs-MBP-2.lan>
2022-05-12 09:24:44 +0700

    Remove Sked Service

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import * as React from 'react'
 
 import { ToastContainer } from 'react-toastify'
-import { withGlobalLoading } from 'shared/dist/components/GlobalLoading'
 import { SWRConfig } from 'swr'
+
 import MainPage from './pages/index'
 import 'react-toastify/dist/ReactToastify.css'
 import { fetchStatusMiddleware } from './hooks/useSWR'
 
 const App: React.FC = () => {
   return (
-      <SWRConfig
-        value={{
-          use: [fetchStatusMiddleware],
-        }}
-      >
-        <MainPage />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          pauseOnHover={true}
-          hideProgressBar={true}
-          className="cx-z-9999"
-        />
-      </SWRConfig>
+    <SWRConfig
+      value={{
+        use: [fetchStatusMiddleware],
+      }}
+    >
+      <MainPage />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        pauseOnHover={true}
+        hideProgressBar={true}
+        className="cx-z-9999"
+      />
+    </SWRConfig>
   )
 }
 
-export default withGlobalLoading(App)
+export default App
